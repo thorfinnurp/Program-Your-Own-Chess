b8b5787744f76fa9020d8aeee997f3b821f8caea
thorfinnurp <thorfinnur16@ru.is>
2021-03-04 21:31:44 +0100

    Pawn movements

--- ChessGUI/src/chessgui/pieces/Pawn.java
+++ ChessGUI/src/chessgui/pieces/Pawn.java
@@ -6,6 +6,8 @@ public class Pawn extends Piece {
 
     private boolean has_moved;
     
+    private Piece p;
+    
     public Pawn(int x, int y, boolean is_white, String file_path, Board board)
     {
         super(x,y,is_white,file_path, board);
@@ -21,6 +23,15 @@ public class Pawn extends Piece {
     {
         return has_moved;
     }
+    public int getXValue()
+    {
+    	return Pawn.this.getX();
+    }
+    public int getYValue()
+    {
+    	return Pawn.this.getY();
+    }
+   
     
     @Override
     public boolean canMove(int destination_x, int destination_y)
@@ -32,9 +43,71 @@ public class Pawn extends Piece {
         // When attacking it may only move space diagonally forward
 
                 // WRITE CODE HERE
-        
+    	
+    	
+    	p = Pawn.this.board.getPiece(destination_x, destination_y);
+    	
+    	if(p == null) 
+    	{
+    		if(getXValue() == destination_x)
+    		{
+		    	if(Pawn.this.isWhite() == true)
+		    	{
+		    	
+		    		if((destination_y == getYValue() + 1) || (destination_y == getYValue() + 2))
+		    		{
+		    			System.out.println("white True!"); 
+		        		return true;
+		    		}
+		    	}
+		    	else {
+		    		
+		    		if((destination_y == getYValue() - 1) || (destination_y == getYValue() - 2))
+		    		{
+		    			System.out.println("Black True!"); 
+		        		return true;
+		    		}
+		    		
+		    	}
+    		}
+    	}
+    	if(Pawn.this.isWhite() == true)
+    	{
+    		if((destination_y == getYValue() + 1) && ((destination_x == getXValue() + 1) || (destination_x == getXValue()-1)) && (p!= null))
+    		{
+    			
+    			if(p.isBlack() == Pawn.this.isWhite())
+    	    	{
+    	    		System.out.println("W - Licence to Kill!");
+    	    		return true;
+    	    	}
+    			
+    		}
+    
+    	}
+    	else
+    	{
+    		if((destination_y == getYValue() - 1) && ((destination_x == getXValue() + 1) || (destination_x == getXValue()-1)) && (p!= null))
+    		{
+    			if(p.isBlack() == Pawn.this.isWhite())
+    	    	{
+    	    		System.out.println("B - Licence to Kill!");
+    	    		return true;
+    	    	}	
+    		}	
+    	}
+    	
+    	
+    	/*if((Pawn.this.isWhite()) & (destination_y == getYValue()+1)) {
+    		
+    		System.out.println("True"); 
+    		return true;
+    	}*/
+    	
+    	System.out.println("Current pos xy: " + getXValue() + getYValue());
+    	System.out.println("Dest pos xy: " + destination_x + destination_y);
                 
-                
-        return true;
+    	System.out.println("Wrong!");       
+        return false;
     }
 }
